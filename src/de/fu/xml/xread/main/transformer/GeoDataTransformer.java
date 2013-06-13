b1d624a8b4d3522b85a8d901c3a5de46d24f1f32
NemoNessuno <gokutwok@gmx.de>
2013-06-13 16:46:53 +0200

    Added dummy classes to fix XReadTest project

---
+++ src/de/fu/xml/xread/main/transformer/GeoDataTransformer.java
@@ -0,0 +1,10 @@
+package de.fu.xml.xread.main.transformer;
+
+public class GeoDataTransformer {
+
+	public static Object GetTemplate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
