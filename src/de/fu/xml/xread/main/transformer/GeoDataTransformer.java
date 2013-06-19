a1c8a1095d627d448f3b421bbeb68946d3cefcad
Monia Hinkelmanns <m_hinkelmanns@web.de>
2013-06-19 17:54:59 +0200

    XSLTTransformer: Code geadded GeoDataTransformer: Code geadded

--- src/de/fu/xml/xread/main/transformer/GeoDataTransformer.java
+++ src/de/fu/xml/xread/main/transformer/GeoDataTransformer.java
@@ -1,10 +1,43 @@
 package de.fu.xml.xread.main.transformer;
 
+import javax.xml.transform.stream.StreamSource;
+
+import de.fu.xml.xread.R;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+/**
+ * @author Monia
+ *
+ */
+
+@SuppressLint("NewApi")
 public class GeoDataTransformer {
 
-	public static Object GetTemplate() {
-		// TODO Auto-generated method stub
-		return null;
+	private static Context context;
+
+	/**
+	 * Konstruktor mit übergebenem ApplicationContext, damit ein Zugriff auf die Ressourcen
+	 * in raw erfolgen kann.
+	 * 
+	 * @param context - Context
+	 */
+	public GeoDataTransformer(Context context) {
+		GeoDataTransformer.context = context;
 	}
 
+	/**
+	 * Greift auf die Ressource geodata_template (XSLT-Datei zur Umwandlung
+	 * von Geodaten) zu und erzeugt einen StreamSource daraus.
+	 * 
+	 * @return - StreamSource
+	 */
+	public static StreamSource GetTemplate() {
+			
+		StreamSource xslt_geo = new StreamSource(context.getResources().openRawResource(R.raw.geodata_template));
+		
+		return xslt_geo;
+		
+	}
 }
