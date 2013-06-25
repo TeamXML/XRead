f5584690da69d08a1dcfb77e468c1d0e875c32b9
NemoNessuno <gokutwok@gmx.de>
2013-06-25 12:35:10 +0200

    why wont you push

---
+++ src/de/fu/xml/xread/main/transformer/Transformer.java
@@ -0,0 +1,30 @@
+package de.fu.xml.xread.main.transformer;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.transform.stream.StreamSource;
+
+import de.fu.xml.xread.main.XSLTTransformer;
+import android.content.Context;
+
+public class Transformer {
+	
+	private GeoDataTransformer _geoDataTransformer;
+	private DefaultTransformer _defaultTransFormer;
+	
+	public Transformer(Context context) {
+		_geoDataTransformer = new GeoDataTransformer(context);
+		_defaultTransFormer = new DefaultTransformer(context);
+	}
+	
+	public String transformGeoData(StreamSource data) throws IOException{
+		return XSLTTransformer.transform(data, _geoDataTransformer.GetTemplate());
+	}
+	
+	public String transFormDefault(InputStream data) throws IOException{		
+		_defaultTransFormer.UpdateXSL(data);
+		return XSLTTransformer.transform(new StreamSource(data), _defaultTransFormer.GetTemplate());
+	}
+	
+}
